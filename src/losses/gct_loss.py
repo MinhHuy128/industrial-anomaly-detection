944454ffb018e755e9adf7de19c5c9872dc1d4f5
HuyMinh <nguyenminhhuy595@gmail.com>
2026-07-28 17:15:00 +0700

    fix gradient leak in loss by detaching backbone CLS token

--- src/losses/gct_loss.py
+++ src/losses/gct_loss.py
@@ -10,7 +10,7 @@ class GlobalConsistencyLoss(nn.Module):
         if gct_output_token.dim() == 3:
             gct_output_token = gct_output_token.squeeze(1)
         # BUG: Missing detach on dinov2_cls_token causing gradient leak into backbone
-        target_cls = dinov2_cls_token
+        target_cls = dinov2_cls_token.detach()
         # Simple MSE loss without projection head
         loss = F.mse_loss(gct_output_token, target_cls)
         return loss
